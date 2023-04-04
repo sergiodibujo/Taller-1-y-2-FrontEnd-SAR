114248c2d95061426eba1e8e7e10da50849a6f51
sergiodibujo <sergiodibujo@gmail.com>
2023-04-04 11:04:23 -0500

    Subí imágenes y el nuevo JsonFossiles.json

--- Taller-1-y-2-FrontEnd-SAR/js/scripts.js
+++ Taller-1-y-2-FrontEnd-SAR/js/scripts.js
@@ -1,15 +1,58 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'caafa2503cmsh4796cd9b6881c8dp10d989jsn3b6201ec5712',
-		'X-RapidAPI-Host': 'animals-by-api-ninjas.p.rapidapi.com'
-	}
-};
+// https://any-api.com/?tag=tools
+// https://www.jsonapi.co/json-queue
+// https://jsonplaceholder.typicode.com/
 
-fetch('https://animals-by-api-ninjas.p.rapidapi.com/v1/animals?name=cheetah', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// https://www.jsonapi.co/public-api/Game%20of%20Thrones%20Quotes
+// https://acnhapi.com/doc#operation/get-bugs-bugID
+// https://api.gbif.org/v1/
+
+
+
+
+
+fetch("https://acnhapi.com/v1/fossils")
+  .then(response => response.json())
+  .then(data => {
+
+	  const dynoArray = data;
+	  console.log(dynoArray)
+
+		const table = document.createElement('table');
+		const tableHeader =  document.createElement('tr');
+		const nameHeader =  document.createElement('th');
+		const descriptionHeader =  document.createElement('th');
+		nameHeader.innerText = "Name";
+		descriptionHeader.innerText = "Description";
+		table.appendChild(tableHeader)
+
+
+		console.log(dynoArray.lenght)
+
+		for(let i = 0 ; i < dynoArray.lenght; i++) {
+
+			const dynoRow = document.createElement('tr');
+			const numberData = document.createElement('td');
+			const nameData = document.createElement('td');
+			const descriptionData = document.createElement('td');
+
+			numberData.innerHTML = i + 1;
+			nameData.innerHTML = dynoArray[i].name;
+			descriptionData.innerHTML = dynoArray[i].Description;
+
+			dynoRow.appendChild(numberData);
+			dynoRow.appendChild(nameData);
+			dynoRow.appendChild(descriptionData);
+
+			table.appendChild(dynoRow)
+		}
+		document.body.appendChild(table)
+
+	}
+)
+  .catch(error => console.log('error', error));
+// .catch(error => console.error('A donde se fueron los dinos?'));
+
+
 
 
 // function buscarDyno () {
@@ -29,7 +72,7 @@ fetch('https://animals-by-api-ninjas.p.rapidapi.com/v1/animals?name=cheetah', op
 //         const descriptionHeader =  document.createElement('th');
 
 //         nameHeader.innerText = "Name";
-//         urlHeader.innerText = "Description";
+//         descriptionHeader.innerText = "Description";
 
 //         table.appendChild(tableHeader)
 
